423addf0906d5637a87d25f536ad8d37516efd23
Harri Feng <harrifeng@gmail.com>
2019-09-21 14:34:32 +0800

    new ok

--- oss.go
+++ oss.go
@@ -2,54 +2,37 @@ package ossutil
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
-func getObjectsFormResponse(lor oss.ListObjectsResult) string {
-	var output string
-	for _, object := range lor.Objects {
-		output += object.Key + " --- "
-	}
-	return output
-}
+// GetObjectTimedURL is used to get oss url which will expire (set by the expireSecond)
+func GetObjectTimedURL(
+	endpoint string,
+	accessID string,
+	accessKey string,
+	bucketName string,
+	objectName string,
+	expireSecond int64,
+) string {
 
-// Hello ...
-func Hello() string {
-
-	endpoint := os.Getenv("ENDPOINT")
-	accessID := os.Getenv("ACCESS_ID")
-	accessKey := os.Getenv("ACCESS_KEY")
 	client, err := oss.New(endpoint, accessID, accessKey)
-	fmt.Println(client)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("client err:", err)
 		return ""
 	}
 
-	bucketName := "xma-rec"
 	bucket, err := client.Bucket(bucketName)
 	if err != nil {
 		fmt.Println("bucket err", err)
 		return ""
 	}
-	fmt.Println("bucket", bucket)
-	lor, err := bucket.ListObjects()
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-	fmt.Println("lor", getObjectsFormResponse(lor))
-
-	url, err := bucket.SignURL("everything.png", oss.HTTPGet, 30)
+	url, err := bucket.SignURL(objectName, oss.HTTPGet, expireSecond)
 
 	if err != nil {
 		fmt.Println(err)
 		return ""
 	}
 
-	fmt.Println("url:", url)
-
-	return "Hello World"
+	return url
 }
--- oss_test.go
+++ oss_test.go
@@ -1,10 +1,27 @@
 package ossutil
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
-	want := "Hello World"
-	if got := Hello(); got != want {
-		t.Errorf("Hello() = %q, want %q", got, want)
+
+	endpoint := os.Getenv("ENDPOINT")
+	accessID := os.Getenv("ACCESS_ID")
+	accessKey := os.Getenv("ACCESS_KEY")
+	bucketName := os.Getenv("BUCKET_NAME")
+	objectName := os.Getenv("OBJECT_NAME")
+
+	want := "http://xma-rec.oss"
+	if got := GetObjectTimedURL(
+		endpoint,
+		accessID,
+		accessKey,
+		bucketName,
+		objectName,
+		30,
+	); got[:len(want)] != want {
+		t.Errorf("GetObjectTimedURL() = %q, want %q", got, want)
 	}
 }
