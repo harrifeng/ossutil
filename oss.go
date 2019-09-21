6d2de1de7f23760081a9dfdb698a707e9f994e48
Harri Feng <harrifeng@gmail.com>
2019-09-21 13:28:32 +0800

    not ok

--- oss.go
+++ oss.go
@@ -1,6 +1,23 @@
 package ossutil
 
+import (
+	"fmt"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
 // Hello ...
 func Hello() string {
+
+	endpoint := ""
+	accessID := ""
+	accessKey := ""
+	client, err := oss.New(endpoint, accessID, accessKey)
+	fmt.Println(client)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
 	return "Hello World"
 }
